b6069639d1e481a673e9cedda63612c1c3ba901f
kadekParwanta <kadek.parwanta@gmail.com>
2017-02-03 15:00:58 +0800

    modify the main build gradle

--- hook/beforePluginInstall.js
+++ hook/beforePluginInstall.js
@@ -8,28 +8,51 @@ module.exports = function (ctx) {
         deferral = ctx.requireCordovaModule('q').defer();
 
     var platformRoot = path.join(ctx.opts.projectRoot, 'platforms/android');
+    var gradleFileLocation = path.join(platformRoot, 'build.gradle');
     var manifestFileLocation = path.join(platformRoot, 'AndroidManifest.xml');
 
-    //Modify AndroidManifest.xml
-    fs.readFile(manifestFileLocation, 'utf-8', function(err, data){
-        var newManifest = data;
-        //add tools namespace
-        if (data.indexOf('http://schemas.android.com/tools') === -1) {
-            newManifest = newManifest.replace('xmlns:android','xmlns:tools=\"http://schemas.android.com/tools\"\nxmlns:android');
-        }
-        
-        //add tools:replace
-        if (newManifest.indexOf('tools:replace=\"android:icon\"') === -1) {
-            newManifest = newManifest.replace('<application','<application tools:replace=\"android:icon\" ');
+    //Modify build.gradle
+    fs.readFile(gradleFileLocation, 'utf-8', function (err, data) {
+        if (err) throw err;
+        var newValue = data;
+
+        //add classpath
+        if (data.indexOf('io.realm:realm-gradle-plugin') === -1) {
+            newValue = newValue.replace('classpath', 'classpath \'io.realm:realm-gradle-plugin:2.1.0\'\nclasspath');
         }
 
-        fs.writeFile(manifestFileLocation, newManifest, 'utf-8', function(err){
+        //add repository
+        if (newValue.indexOf('mavenCentral()') === -1) {
+            newValue = newValue.replace('jcenter()','mavenCentral()\njcenter()')
+        }
+        
+
+        fs.writeFile(gradleFileLocation, newValue, 'utf-8', function (err) {
             if (err) throw err;
-            console.log('AndroidManifest.xml complete');
-            deferral.resolve();
-        })
-    })
-    
+            console.log('build.gradle complete');
+
+            //Modify AndroidManifest.xml
+            fs.readFile(manifestFileLocation, 'utf-8', function(err, data){
+                var newManifest = data;
+                //add tools namespace
+                if (data.indexOf('http://schemas.android.com/tools') === -1) {
+                    newManifest = newManifest.replace('xmlns:android','xmlns:tools=\"http://schemas.android.com/tools\"\nxmlns:android');
+                }
+                
+                //add tools:replace
+                if (newManifest.indexOf('tools:replace=\"android:icon\"') === -1) {
+                    newManifest = newManifest.replace('<application','<application tools:replace=\"android:icon\" ');
+                }
+
+                fs.writeFile(manifestFileLocation, newManifest, 'utf-8', function(err){
+                    if (err) throw err;
+                    console.log('AndroidManifest.xml complete');
+                    deferral.resolve();
+                })
+            })
+            
+        });
+    });
 
     return deferral.promise;
 };
